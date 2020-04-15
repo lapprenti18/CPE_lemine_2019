e19a1bc268bdbcad9c648b7d02de7127f3d0c530
Gabriel <gabriel.knies@epitech.eu>
2020-04-15 17:29:56 +0200

    feat(master) : fixing : nb of ants is now improved

--- include/my.h
+++ include/my.h
@@ -40,5 +40,6 @@ int parser(lemin_t *lemin, char *str);
 void fill_neigh(node_t **head, int distance);
 void print_rooms(node_t *head);
 void mouve_ant(int nb_of_ants, char **tab);
+void get_nb_ants(lemin_t *lemin, char *tab[]);
 
 #endif /* !MY_H_ */
